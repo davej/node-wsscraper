b9e5c9dbca70cc7a1cf8e1aefa9e9ca658c44ca7
davej <dave@davejeffery.com>
2011-01-12 13:43:57 +0000

    Updating docs and examples

--- examples/json.js
+++ examples/json.js
@@ -3,6 +3,6 @@ var scraper = require('wsscraper');
 scraper('http://search.twitter.com/search.json?q=javascript', 'json', function(err, json_object, urlInfo) {
 	if (err) {throw err;}
     for (var i=0; i < json_object.results.length; i++) {
-        console.log(i+1 + ": " + json_object.results[i].text);
+        console.log(json_object.results[i].text+'\n');
     };
 });
--- examples/xml.js
+++ examples/xml.js
@@ -3,6 +3,6 @@ var scraper = require('wsscraper');
 scraper('http://search.twitter.com/search.atom?q=javascript', 'xml', function(err, json_object, urlInfo) {
 	if (err) {throw err;}
     for (var i=0; i < json_object.entry.length; i++) {
-        console.log(i+1 + ": " + json_object.entry[i].title)
+        console.log(json_object.entry[i].title+'\n')
     };
 });
